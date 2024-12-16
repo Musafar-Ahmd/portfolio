791aa26f0598a0b28e51c484c6b572418b057171
musafarahamed <musafar.innerix@gmail.com>
2024-12-16 20:19:41 +0530

    new changes

--- lib/widgets/main_mobile.dart
+++ lib/widgets/main_mobile.dart
@@ -1,9 +1,21 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../constants/app_text_styles.dart';
 import '../constants/colors.dart';
 
-class MainMobile extends StatelessWidget {
+class MainMobile extends StatefulWidget {
   const MainMobile({super.key});
+
+  @override
+  State<MainMobile> createState() => _MainMobileState();
+}
+
+class _MainMobileState extends State<MainMobile> {
+  final _url =
+      'https://drive.google.com/file/d/1rUd89CZjV0R34_uopZYTBK42bNaCJxSw/view?usp=sharing';
 
   @override
   Widget build(BuildContext context) {
@@ -40,26 +52,75 @@ class MainMobile extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           // intro message
-          const Text(
-            "Hi,\nI'm Musafar Ahamed\nA Flutter Developer",
-            style: TextStyle(
-              fontSize: 24,
-              height: 1.5,
-              fontWeight: FontWeight.bold,
-              color: CustomColor.whitePrimary,
-            ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeInDown(
+                duration: const Duration(milliseconds: 1200),
+                child: Text(
+                  'Hello, It\'s Me',
+                  style: AppTextStyles.montserratStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 15.0),
+              FadeInRight(
+                duration: const Duration(milliseconds: 1400),
+                child: Text(
+                  'Musafar Ahamed',
+                  style: AppTextStyles.headingStyles(),
+                ),
+              ),
+              FadeInLeft(
+                duration: const Duration(milliseconds: 1400),
+                child: Row(
+                  children: [
+                    Text(
+                      'And I\'m a ',
+                      style: AppTextStyles.montserratStyle(color: Colors.white),
+                    ),
+                    AnimatedTextKit(
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          'Flutter Developer',
+                          textStyle: AppTextStyles.montserratStyle(
+                              color: Colors.lightBlue),
+                        ),
+                        TyperAnimatedText('Freelancer',
+                            textStyle: AppTextStyles.montserratStyle(
+                                color: Colors.lightBlue)),
+                      ],
+                      pause: const Duration(milliseconds: 1000),
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true,
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: 250.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _launchURL(_url);
+                  },
+                  child: const Text(
+                    "Download CV",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              )
+            ],
           ),
-          const SizedBox(height: 15),
-          // contact btn
-          SizedBox(
-            width: 190.0,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text("Get in touch"),
-            ),
-          )
         ],
       ),
     );
+  }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
